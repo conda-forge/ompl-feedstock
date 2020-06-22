84a812a80a4e38762bd061ab7de2308cb1e060da
rstrudel <rstrudel@gmail.com>
2020-06-22 15:31:27 +0200

    Trying to add python bindings

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+set -x
 
 mkdir build && cd build
+
+# python dependencies
+pip install -vU https://github.com/CastXML/pygccxml/archive/develop.zip pyplusplus
+# castxml
+wget -q -O- https://data.kitware.com/api/v1/file/5e8b740d2660cbefba944189/download | tar zxf - -C ${HOME}
+export PATH=${HOME}/castxml/bin:${PATH}
+
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DOMPL_BUILD_DEMOS=OFF \
+    -DPYTHON_EXEC=${PYTHON}\
       $SRC_DIR
 
 VERBOSE=1 make -j${CPU_COUNT}
