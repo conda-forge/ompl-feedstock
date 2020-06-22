cff3c70388c6ec20390c25d00ea73ef4c675968e
Loïc Estève <loic.esteve@ymail.com>
2020-06-22 17:35:38 +0200

    Need boost.Python to build Python bindings and castxml

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,11 @@ mkdir build && cd build
 
 # python dependencies
 pip install -vU https://github.com/CastXML/pygccxml/archive/develop.zip
-pip3 install https://files.pythonhosted.org/packages/aa/ed/445b49e7ce743e314b4fc631c25aec366f11837cf451a5417feefede4196/pyplusplus-1.8.3-py3-none-any.whl
+pip install https://files.pythonhosted.org/packages/aa/ed/445b49e7ce743e314b4fc631c25aec366f11837cf451a5417feefede4196/pyplusplus-1.8.3-py3-none-any.whl
 
 # castxml
-# wget -q -O- https://data.kitware.com/api/v1/file/5e8b740d2660cbefba944189/download | tar zxf - -C ${HOME}
-# export PATH=${HOME}/castxml/bin:${PATH}
+wget -q -O- https://data.kitware.com/api/v1/file/5e8b740d2660cbefba944189/download | tar zxf - -C ${HOME}
+export PATH=${HOME}/castxml/bin:${PATH}
 
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
