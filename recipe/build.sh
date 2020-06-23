6639df2c320ea749dc646ecefadee74037b1c37e
Loïc Estève <loic.esteve@ymail.com>
2020-06-23 15:26:19 +0200

    Add pypy to speed-up Python bindings generation

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,11 +11,14 @@ pip install https://files.pythonhosted.org/packages/aa/ed/445b49e7ce743e314b4fc6
 wget -q -O- https://data.kitware.com/api/v1/file/5e8b740d2660cbefba944189/download | tar zxf - -C ${HOME}
 export PATH=${HOME}/castxml/bin:${PATH}
 
+# pypy to speed-up Python bindings
+wget -q -O- https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.3.1-linux64.tar.bz2 | tar jxf -
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
 	  -DCMAKE_INSTALL_LIBDIR=lib \
 	  -DOMPL_BUILD_DEMOS=OFF \
+          -DPYPY=/pypy3.6-v7.3.1-linux64/bin/pypy3 \
           -D-DPYTHON_EXEC=$PYTHON \
           -DOMPL_BUILD_PYTESTS=OFF \
           -DOMPL_BUILD_PYBINDINGS=ON \
