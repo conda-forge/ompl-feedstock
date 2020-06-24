608e6c3ff4541a861914e61487c4b7c817615394
Loïc Estève <loic.esteve@ymail.com>
2020-06-24 10:55:09 +0200

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,13 @@ set -ex
 
 mkdir build && cd build
 
+conda info -e
+echo $PYTHON
+conda list
+
 # python dependencies
-pip install -vU https://github.com/CastXML/pygccxml/archive/develop.zip
-pip install https://files.pythonhosted.org/packages/aa/ed/445b49e7ce743e314b4fc631c25aec366f11837cf451a5417feefede4196/pyplusplus-1.8.3-py3-none-any.whl
+$PYTHON -m pip install -vU https://github.com/CastXML/pygccxml/archive/develop.zip
+$PYTHON -m pip install https://files.pythonhosted.org/packages/aa/ed/445b49e7ce743e314b4fc631c25aec366f11837cf451a5417feefede4196/pyplusplus-1.8.3-py3-none-any.whl
 
 # castxml
 wget -q -O- https://data.kitware.com/api/v1/file/5e8b740d2660cbefba944189/download | tar zxf - -C ${HOME}
@@ -13,8 +17,6 @@ export PATH=${HOME}/castxml/bin:${PATH}
 
 # pypy to speed-up Python bindings
 wget -q -O- https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.3.1-linux64.tar.bz2 | tar jxf -
-
-env
 
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
 	  -DCMAKE_BUILD_TYPE=Release \
