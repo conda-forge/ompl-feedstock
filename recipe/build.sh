a78df5b4692fd20e4cbb7810b165d4cb1040ca39
David Dorf <daviddorf2023@u.northwestern.edu>
2025-06-27 18:32:06 -0400

    CI tests workarounds reintroduction

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,4 +14,18 @@ cmake "${CMAKE_ARGS}" \
 
 ninja
 ninja install
-# ninja update_bindings  ## ninja: error: unknown target 'update_bindings' ##
+# ninja update_bindings  ## "ninja: error: unknown target 'update_bindings'" ##
+
+# Disable certain tests to pass CI
+if [[ "${target_platform}" == osx-* ]]; then
+  # This is a workaround for https://github.com/conda-forge/ompl-feedstock/issues/43,
+  # check the discussion there for more details
+  export CTEST_DISABLED_TESTS="test_planner_data"
+else
+  export CTEST_DISABLED_TESTS=""
+fi
+
+if [[ "$target_platform" != "linux-aarch64" && "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+	# run tests, currently failing on arm64 for some reason
+	ctest --progress --output-on-failure -E "${CTEST_DISABLED_TESTS}"
+fi
